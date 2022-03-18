e73467306723e23888974857f41d5890f38259f5
NINjarobby <adwini.robert@gmail.com>
2022-03-18 08:36:36 +0000

    ...

---
+++ FimiEngine/HelpingClasses/ServiceObject.cs
@@ -0,0 +1,8 @@
+namespace FimiEngine.HelpingClasses;
+
+public class ServiceObject
+{
+    public string OS{ get; set; }
+    public string ServiceName { get; set; }
+    public string MachineName { get; set; }
+}
